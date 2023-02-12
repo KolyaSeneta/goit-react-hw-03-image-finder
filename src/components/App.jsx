52467c9589734714f00ccbdcb9f608394ed8c07d
KolyaSeneta <114915582+KolyaSeneta@users.noreply.github.com>
2023-02-12 23:11:20 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,107 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Api } from './Api/Api';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import Modal from './Modal/Modal';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+
+export class App extends Component {
+  state = {
+    images: [],
+    isLoading: false,
+    currentSearch: '',
+    pageNum: 1,
+    modalOpen: false,
+    modalImg: '',
+    modalAlt: '',
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ isLoading: false });
+    const inputForSearch = e.target.elements.inputForSearch;
+    if (inputForSearch.value.trim() === '') {
+      return toast.error(' Please enter the text!');
+    } else {
+      this.setState({ isLoading: true });
+    }
+    
+    const response = await  Api(inputForSearch.value, 1);
+    this.setState({
+      images: response,
+      isLoading: false,
+      currentSearch: inputForSearch.value,
+      pageNum: 1,
+    });
+  };
+
+  handleClickMore = async () => {
+    const response = await  Api(
+      this.state.currentSearch,
+      this.state.pageNum + 1
+    );
+    this.setState({
+      images: [...this.state.images, ...response],
+      pageNum: this.state.pageNum + 1,
+    });
+  };
+
+  handleImageClick = e => {
+    this.setState({
+      modalOpen: true,
+      modalAlt: e.target.alt,
+      modalImg: e.target.name,
+    });
+  };
+
+  handleModalClose = () => {
+    this.setState({
+      modalOpen: false,
+      modalImg: '',
+      modalAlt: '',
+    });
+  };
 
 
 
-class App extends Component {
-   
 
   render() {
     return (
-      <div>
-        
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gridGap: '16px',
+          paddingBottom: '24px',
+        }}
+      >
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <Searchbar onSubmit={this.handleSubmit} />
+            <ImageGallery
+              onImageClick={this.handleImageClick}
+              images={this.state.images}
+            />
+            {this.state.images.length > 0 ? (
+              <Button onClick={this.handleClickMore} />
+            ) : null}
+          </React.Fragment>
+        )}
+        {this.state.modalOpen ? (
+          <Modal
+            src={this.state.modalImg}
+            alt={this.state.modalAlt}
+            handleClose={this.handleModalClose}
+          />
+        ) : null}
+        <ToastContainer />
       </div>
-    )
+    );
   }
 }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,36 @@
+import propTypes from 'prop-types';
+import css from './Modal.module.css';
+import { Component } from 'react';
+
+export default class Modal extends Component {
+    componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+    handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.handleModalClose();
+    }
+  };
+  render() {
+    const { src, alt, handleClose } = this.props;
+
+    return (
+       <div className={css.Overlay} onClick={handleClose}>
+       <div className={css.Modal}>
+      <img src={src} alt={alt} />
+        </div>
+      </div>
+    );
+  }
+}
+ 
+
+Modal.propTypes = {
+  src: propTypes.string.isRequired,
+  alt: propTypes.string.isRequired,
+  handleClose: propTypes.func.isRequired,
+};
